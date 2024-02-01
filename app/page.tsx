98ac821df0c5b2484c12f5504c3a821814b7176a
aboveyunhai <35160613+aboveyunhai@users.noreply.github.com>
2024-02-01 14:04:21 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { ShikiRender } from '@/components/shiki-render';
-import { ShikiUseEffect } from '@/components/shiki-use-effect';
+import { ShikiRender } from "@/components/shiki-render";
+import { ShikiUseEffect } from "@/components/shiki-use-effect";
 
 const react_code = `
 import React, { useState } from "react"
@@ -17,16 +17,17 @@ console.log("Hi! Shiki + Twoslash on CDN :)")
 
 const count = ref(0)
 //     ^?
-`.trim()
+`.trim();
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-5 items-center justify-between p-5">
       <div>Abuse</div>
-      <ShikiRender code={react_code} lang={'tsx'} />
-      <ShikiRender code={vue_code} lang={'ts'} />
+      <ShikiRender code={react_code} lang={"tsx"} />
+      <ShikiRender code={vue_code} lang={"ts"} />
       <div>useEffect</div>
-      <ShikiUseEffect code={react_code} lang={'tsx'}/>
+      <ShikiUseEffect code={react_code} lang={"tsx"} />
+      <ShikiUseEffect code={vue_code} lang={"ts"} />
     </main>
   );
 }
